36a4a05079546ad1a9cbc8955d830c6b74fbee78
MikePio <michelepiopilla@gmail.com>
2023-05-16 20:03:35 +0200

    Print hotel details from a multidimensional array using a double loop forEach

--- index.php
+++ index.php
@@ -1,21 +1,78 @@
 <?php
 
-$test = 'test!';
+$hotels = [
+
+  [
+    'name' => 'Hotel Belvedere',
+    'description' => 'Hotel Belvedere Descrizione',
+    'parking' => true,
+    'vote' => 4,
+    'distance_to_center' => 10.4
+  ],
+  [
+    'name' => 'Hotel Futuro',
+    'description' => 'Hotel Futuro Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 2
+  ],
+  [
+    'name' => 'Hotel Rivamare',
+    'description' => 'Hotel Rivamare Descrizione',
+    'parking' => false,
+    'vote' => 1,
+    'distance_to_center' => 1
+  ],
+  [
+    'name' => 'Hotel Bellavista',
+    'description' => 'Hotel Bellavista Descrizione',
+    'parking' => false,
+    'vote' => 5,
+    'distance_to_center' => 5.5
+  ],
+  [
+    'name' => 'Hotel Milano',
+    'description' => 'Hotel Milano Descrizione',
+    'parking' => true,
+    'vote' => 2,
+    'distance_to_center' => 50
+  ],
+
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP HOTEL</title>
-  </head>
-  <body>
-  
-  <h2><?php echo $test ?></h2>
-  <h2>test</h2>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- BOOTSTRAP -->
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">   -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+  <title>PHP HOTEL</title>
+</head>
+
+<body class="bg-dark text-white">
+
+  <div class="container p-5">
+
+  <div><?php foreach ($hotels as $hotel){
+    // echo $hotel; // non funziona
+    foreach($hotel as $hotelDetails){
+      echo $hotelDetails . "<br>";
+    }
+  } ?></div>
+
+
+
+  </div>
+
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script> -->
+
 
 </body>
+
 </html>
